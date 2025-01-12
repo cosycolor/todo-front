2c05f1bf0475852b028094340abdc33261fb43ab
cosycolor <handyoon87@gmail.com>
2025-01-12 23:51:58 +0900

    add delete, update

--- src/App.js
+++ src/App.js
@@ -15,12 +15,21 @@ function App() {
       console.log("items", items);
     }
 
+    const deleteItem = (item) =>{
+      const newItems = items.filter(e => e.id !== item.id);
+      setItems([...newItems]);
+    }
+
+    const editItem = () =>{
+      setItems([...items]);
+    }
+
     let todoItems = 
       items.length > 0 && (
         <Paper style = {{margin : 16}}>
           <List>
             {items.map((item) => (
-              <Todo item = {item} key = {item.id}/>
+              <Todo item = {item} key = {item.id} deleteItem={deleteItem} editItem={editItem}/>
             ))}
           </List>
         </Paper>
